d8f7d706bfc402e38be878a4341926fba6453a5d
Jashan <Jashan@DESKTOP-FGLV6P9>
2022-04-16 03:53:54 -0700

    first commit in jashan-04 branch

--- HelloDotNetCore/Program.cs
+++ HelloDotNetCore/Program.cs
@@ -8,6 +8,7 @@ namespace HelloDotNetCore
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Again!");
+            Console.WriteLine("Hello From Jashan-04 branch");
         }
     }
 }
