94770a93e71aa8d240fc2b05b8d7fcf005756b67
Jashan <Jashan@DESKTOP-FGLV6P9>
2022-04-16 03:48:58 -0700

    Add project files.

---
+++ HelloDotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloDotNetCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
